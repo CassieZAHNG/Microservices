f62570cb3314699b33a49437efbde773a6c8bbfb
CassieZAHNG <cassiebetter@gmail.com>
2022-03-13 18:55:04 +1100

    Setup db, models and repository

---
+++ Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext db;
+        private IMapper mapper;
+
+        public ProductRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        public Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> GetProdcutById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
