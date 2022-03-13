7d15bea1a78aadd694a193014f6cc9c9caaa7da6
CassieZAHNG <cassiebetter@gmail.com>
2022-03-13 22:20:55 +1100

    add httpclient configuration and product service DI

--- Mongo.Web/Startup.cs
+++ Mongo.Web/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mongo.Web.Services;
+using Mongo.Web.Services.IServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,10 @@ namespace Mongo.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IProductService, ProductService>();
+            SD.ProductAPIBase = Configuration["ServiceUrls:ProductAPI"];
+
+            services.AddScoped<IProductService, ProductService>();
             services.AddControllersWithViews();
         }
 
